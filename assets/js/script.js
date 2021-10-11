c087d7d0703007e43d1cf141ab6639485712e908
Luis Fernando Jimenez <lu.jim@protonmail.com>
2021-10-11 16:22:02 -0400

    Add dynamic nav menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-document.querySelector('.btn').onclick = function (e) {
-  var menu = document.querySelector('.menu');
-  var btn = document.querySelector('.btn');
+// document.querySelector('.btn').onclick = function (e) {
+//   var menu = document.querySelector('.menu');
+//   var btn = document.querySelector('.btn');
 
-  menu.classList.toggle('is-active');
-  btn.classList.toggle('is-active');
+//   menu.classList.toggle('is-active');
+//   btn.classList.toggle('is-active');
 
-  e.preventDefault();
+//   e.preventDefault();
+// }
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 }

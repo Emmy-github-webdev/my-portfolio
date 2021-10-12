65289b473b26507aa011cdd7d31a9c8e679a7302
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-10-12 01:44:42 +0100

    Check lint error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,7 @@
-// document.querySelector('.btn').onclick = function (e) {
-//   var menu = document.querySelector('.menu');
-//   var btn = document.querySelector('.btn');
-
-//   menu.classList.toggle('is-active');
-//   btn.classList.toggle('is-active');
-
-//   e.preventDefault();
-// }
-
-/* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById('myNav').style.width = '100%';
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById('myNav').style.width = '0%';
 }
